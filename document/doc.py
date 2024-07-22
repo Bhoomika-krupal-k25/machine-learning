78e078aba0ba0162d96582e1ae293f92e15efdfc
Bhoomika-krupal-k25 <142391907+Bhoomika-krupal-k25@users.noreply.github.com>
2024-07-22 20:20:46 +0530

    doc.py

--- document/doc.py
+++ document/doc.py
@@ -1,47 +1,48 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.pipeline import Pipeline
-# from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import SGDClassifier
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# import numpy as np
+import sys
+from sklearn.feature_extraction import text
+from sklearn import pipeline
+from sklearn import linear_model
+import numpy as np
 
+l = [
+    ('Business means risk!', 1),
+    ("This is a document", 1),
+    ("this is another document", 4),
+    ("documents are separated by newlines", 8)
+]
 
-# Load in training data
+def load_data(filename):
+    with open(filename, 'r') as data_file:
+        s = int(data_file.readline())
+        X = np.zeros(s, dtype=np.object_)
+        Y = np.zeros(s, dtype=np.int_)
+        for i, line in enumerate(data_file):
+            ind = line.index(' ')
+            if ind == -1:
+                raise ValueError('invalid input file')
+            targ = int(line[:ind])
+            words = line[ind + 1:]
+            X[i] = words
+            Y[i] = targ
+    return X, Y
 
-fname = 'trainingdata.txt'
+# Load training data
+X, Y = load_data('trainingdata.txt')
 
-targets = []
-data    = [] 
-
-with open(fname) as f:
-    for line in f:
-        targets.append(line[0])
-        data.append(line[2:])
-
-# train the classifier
-
-textClf = Pipeline([('vect', CountVectorizer(max_df = 8.5)),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', SGDClassifier()),
+c = pipeline.Pipeline([
+    ('vect', text.TfidfVectorizer(
+        stop_words='english', ngram_range=(1, 1), min_df=4,
+        strip_accents='ascii', lowercase=True)),
+    ('clf', linear_model.SGDClassifier(class_weight='balanced'))
 ])
-_ = textClf.fit(data,targets)
+model = c.fit(X, Y)
 
+t = list(line for line in sys.stdin)[1:]
 
-# load in test data
-
-docs = []
-for _ in range(input()):
-    docs.append(raw_input().strip())
-
-# predict
-
-predicted = textClf.predict(docs)
-
-# output in hackerrank format
-
-for pred in predicted:
-    print pred
-docs = []
-for _ in range(input()):
-    docs.append(raw_input().strip())
+for y, x in zip(model.predict(t), t):
+    for pattern, targ in l:
+        if pattern in x:
+            print(targ)
+            break
+    else:
+        print(y)
